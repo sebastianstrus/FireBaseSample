efb1c4647a076eafecad6802f17c5570c0292003
Sebastian Strus <sebastian.strus@opensolution.se>
2018-06-17 01:32:42 +0200

    Added Strings

---
+++ FireBaseSample/Extensions/String+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extension.swift
+//  FireBaseSample
+//
+//  Created by Sebastian Strus on 2018-06-17.
+//  Copyright © 2018 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+// use:     myLabel.text = "Hi".localized()
+//          myLabel.text = "Hi".localized(withComment: "with !!!")
+
+extension String {
+    func localized(withComment comment: String? = nil) -> String {
+        return NSLocalizedString(self, comment: comment ?? "")
+    }
+    
+}
---
+++ FireBaseSample/Resources/Strings/Strings.swift
@@ -0,0 +1,16 @@
+//
+//  Strings.swift
+//  FireBaseSample
+//
+//  Created by Sebastian Strus on 2018-06-17.
+//  Copyright © 2018 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+struct Strings {
+    // Error Messages
+    static let hello_sebastian = "Hello Sebastian Strus".localized()
+    static let download_error = "Error in Download".localized()
+    
+}
